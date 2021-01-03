461716037e0bcd00630f976b8769a882f63e991e
Ramin Anvar <ramina1964@gmail.com>
2021-01-03 01:03:08 +0100

    The same message as for the previous commit

--- SearchMethods.GUI/ViewModel/MainViewModel.cs
+++ SearchMethods.GUI/ViewModel/MainViewModel.cs
@@ -7,292 +7,328 @@ using System.Windows;
 using SearchMethods.Model;
 using SearchMethods.Model.Properties;
 using GalaSoft.MvvmLight.Command;
+using System.Diagnostics;
 
 namespace SearchMethods.GUI.ViewModel
 {
-	/********************************************* Constructors ********************************************/
-	public class MainViewModel : ExtendedViewModelBase
-	{
-		public MainViewModel(ISearchItem searchItem)
-		{
-			SearchItem = searchItem;
-			SimulateCommand = new RelayCommand(Simulate, CanSimulate);
-			CancelCommand = new RelayCommand(Cancel, CanCancel);
+    /********************************************* Constructors ********************************************/
+    public class MainViewModel : ExtendedViewModelBase
+    {
+        public MainViewModel(ISearchItem searchItem)
+        {
+            SearchItem = searchItem;
+            SimulateCommand = new RelayCommand(Simulate, CanSimulate);
+            CancelCommand = new RelayCommand(Cancel, CanCancel);
 
-			NoOfEntries = (int)5e5;
-			NoOfSearches = (int)1e3;
+            NoOfEntries = (int)5e5;
+            NoOfSearches = (int)1e3;
 
-			IsSimulating = false;
-			ProgressBarVisibility = Visibility.Collapsed;
-		}
+            IsSimulating = false;
+            ProgressBarVisibility = Visibility.Collapsed;
+        }
 
-		/************************************** Static and Constants ***************************************/
-		public static long MinProductValue = (long)1e5;
-		public static long MaxProductValue = (long)5e10;
-		public static readonly string MaxProductError =
-			$"Product of No. of searches and No. of entries must be in the interval [{MinProductValue}, {MaxProductValue}].";
+        /************************************** Static and Constants ***************************************/
+        public static long MinProductValue = (long)1e5;
+        public static long MaxProductValue = (long)5e10;
+        public static readonly string MaxProductError =
+            $"Product of No. of searches and No. of entries must be in the interval [{MinProductValue}, {MaxProductValue}].";
 
-		/************************************ Public Events & Delegates ************************************/
-		public RelayCommand  SimulateCommand { get; set; }
+        /************************************ Public Events & Delegates ************************************/
+        public RelayCommand SimulateCommand { get; set; }
 
-		public RelayCommand CancelCommand { get; set; }
+        public RelayCommand CancelCommand { get; set; }
 
-		/******************************************* Properties ********************************************/
-		public ISearchItem SearchItem { get; set; }
+        /******************************************* Properties ********************************************/
+        public ISearchItem SearchItem { get; set; }
 
-		public bool IsSimulating
-		{
-			get => _isSimulating;
-			set => Set(ref _isSimulating, value);
-		}
+        public bool IsSimulating
+        {
+            get => _isSimulating;
+            set => Set(ref _isSimulating, value);
+        }
 
-		public bool IsIdle => !IsSimulating;
+        public bool IsIdle => !IsSimulating;
 
-		public Visibility ProgressBarVisibility
-		{
-			get => _progressBarVisibility;
-			set => Set(ref _progressBarVisibility, value);
-		}
+        public Visibility ProgressBarVisibility
+        {
+            get => _progressBarVisibility;
+            set => Set(ref _progressBarVisibility, value);
+        }
 
-		public double ProgressBarValue
-		{
-			get => _progressBarValue;
-			set => Set(ref _progressBarValue, value);
-		}
+        public double ProgressBarValue
+        {
+            get => _progressBarValue;
+            set => Set(ref _progressBarValue, value);
+        }
 
-		public string ProgressBarText => Math.Round(ProgressBarValue, 0) + "%";
+        public string ProgressBarText => Math.Round(ProgressBarValue, 0) + "%";
 
-		public ISimulationResults LinearSearchResults { get; set; }
-		public ISimulationResults BinarySearchResults { get; set; }
+        public ISimulationResults LinearSearchResults { get; set; }
 
-		public int NoOfEntries
-		{
-			get => _noOfEntries;
-			set => Set(ref _noOfEntries, value);
-		}
+        public ISimulationResults BinarySearchResults { get; set; }
 
-		public int NoOfSearches
-		{
-			get => _noOfSearches;
-			set => Set(ref _noOfSearches, value);
-		}
+        public int NoOfEntries
+        {
+            get => _noOfEntries;
+            set => Set(ref _noOfEntries, value);
+        }
 
-		public int TargetValue
-		{
-			get => _targetValue;
+        public int NoOfSearches
+        {
+            get => _noOfSearches;
+            set => Set(ref _noOfSearches, value);
+        }
 
-			set
-			{
-				if (Set(ref _targetValue, value))
-				{ TargetIndex = BinarySearchType.FindItem(value).TargetIndex; }
-			}
-		}
+        public int TargetValue
+        {
+            get => _targetValue;
 
-		public int? TargetIndex
-		{
-			get => _targetIndex;
-			set => Set(ref _targetIndex, value);
-		}
+            set
+            {
+                if (Set(ref _targetValue, value))
+                { TargetIndex = BinarySearch.FindItem(value).TargetIndex; }
+            }
+        }
 
-		public string Entries { get; set; }
+        public int? TargetIndex
+        {
+            get => _targetIndex;
+            set => Set(ref _targetIndex, value);
+        }
 
-		private LinearSearch LinearSearchType { get; set; }
+        public string Entries { get; set; }
 
-		private BinarySearch BinarySearchType { get; set; }
+        public double BinaryAvgNoOfIterations
+        {
+            get => _binaryAvgNoOfIterations;
+            set => Set(ref _binaryAvgNoOfIterations, value);
+        }
 
+        public double BinaryAvgElapsedTime
+        {
+            get => _binaryAvgElapsedTime;
+            set => Set(ref _binaryAvgElapsedTime, value);
+        }
 
-		/***************************************** Private Methods *****************************************/
-		private void Validate()
-		{
-			Task.Run(() => DataValidation());
-		}
+        public double LinearAvgNoOfIterations
+        {
+            get => _linearAvgNoOfIterations;
+            set => Set(ref _linearAvgNoOfIterations, value);
+        }
 
-		private void DataValidation()
-		{
-			#region NoOfEntries
-			//Validate No. of Entries property
-			if (PropErrors.TryGetValue(nameof(NoOfEntries), out var listErrors) == false)
-				listErrors = new List<string>();
+        public double LinearAvgElapsedTime
+        {
+            get => _linearAvgElapsedTime;
+            set => Set(ref _linearAvgElapsedTime, value);
+        }
 
-			else
-				listErrors.Clear();
+        /***************************************** Private Methods *****************************************/
+        private void Validate()
+        {
+            Task.Run(() => DataValidation());
+        }
 
-			if (string.IsNullOrEmpty(nameof(NoOfEntries)))
-				listErrors.Add("No. of Entries is required.");
+        private void DataValidation()
+        {
+            #region NoOfEntries
+            //Validate No. of Entries property
+            if (PropErrors.TryGetValue(nameof(NoOfEntries), out var listErrors) == false)
+                listErrors = new List<string>();
 
-			if (Settings.Default.MinNoOfEntries > NoOfEntries || NoOfEntries > Settings.Default.MaxNoOfEntries)
-				listErrors.Add(Resources.NoOfEntriesTooSmallError);
+            else
+                listErrors.Clear();
 
-			PropErrors[nameof(NoOfEntries)] = listErrors;
+            if (string.IsNullOrEmpty(nameof(NoOfEntries)))
+                listErrors.Add("No. of Entries is required.");
 
-			if (listErrors.Count > 0)
-			{
-				OnPropertyErrorsChanged(nameof(NoOfEntries));
-			}
-			#endregion NoOfEntries
+            if (Settings.Default.MinNoOfEntries > NoOfEntries || NoOfEntries > Settings.Default.MaxNoOfEntries)
+                listErrors.Add(Resources.NoOfEntriesTooSmallError);
 
-			#region NoOfSearches
-			//Validate No. of Searches property
-			if (PropErrors.TryGetValue(nameof(NoOfSearches), out listErrors) == false)
-				listErrors = new List<string>();
+            PropErrors[nameof(NoOfEntries)] = listErrors;
 
-			else
-				listErrors.Clear();
+            if (listErrors.Count > 0)
+            {
+                OnPropertyErrorsChanged(nameof(NoOfEntries));
+            }
+            #endregion NoOfEntries
 
-			if (string.IsNullOrEmpty(nameof(NoOfSearches)))
-				listErrors.Add("No. of Searches is required.");
+            #region NoOfSearches
+            //Validate No. of Searches property
+            if (PropErrors.TryGetValue(nameof(NoOfSearches), out listErrors) == false)
+                listErrors = new List<string>();
 
-			if (Settings.Default.MinNoOfSearches > NoOfSearches || NoOfSearches > Settings.Default.MaxNoOfSearches)
-				listErrors.Add(Resources.NoOfSearchesTooLargeError);
+            else
+                listErrors.Clear();
 
-			PropErrors[nameof(NoOfSearches)] = listErrors;
+            if (string.IsNullOrEmpty(nameof(NoOfSearches)))
+                listErrors.Add("No. of Searches is required.");
 
-			if (listErrors.Count > 0)
-			{
-				OnPropertyErrorsChanged(nameof(NoOfSearches));
-			}
-			#endregion NoOfSearches
-		}
+            if (Settings.Default.MinNoOfSearches > NoOfSearches || NoOfSearches > Settings.Default.MaxNoOfSearches)
+                listErrors.Add(Resources.NoOfSearchesTooLargeError);
 
-		private bool CanSimulate()
-		{
-			return IsIdle && IsInputValid();
-		}
+            PropErrors[nameof(NoOfSearches)] = listErrors;
 
-		private bool CanCancel()
-		{
-			return IsIdle && IsInputValid();
-		}
+            if (listErrors.Count > 0)
+            {
+                OnPropertyErrorsChanged(nameof(NoOfSearches));
+            }
+            #endregion NoOfSearches
+        }
 
-		private void Simulate()
-		{
-			IsSimulating = true;
-			ProgressBarVisibility = Visibility.Visible;
-			LinearSearchType = new LinearSearch(SearchItem, NoOfEntries);
-			BinarySearchType = new BinarySearch(SearchItem, NoOfEntries);
-			var searches = new BaseSearch[] { LinearSearchType, BinarySearchType };
+        private bool CanSimulate()
+        {
+            return IsIdle && IsInputValid();
+        }
 
-			new Thread(() => Simulate(searches)).Start();
-			IsSimulating = false;
-		}
+        private bool CanCancel()
+        {
+            return IsIdle && IsInputValid();
+        }
 
-		private void Cancel()
-		{
+        private void Simulate()
+        {
+            IsSimulating = true;
+            ProgressBarVisibility = Visibility.Visible;
+            LinearSearch = new LinearSearch(SearchItem, NoOfEntries);
+            BinarySearch = new BinarySearch(SearchItem, NoOfEntries);
+            var searches = new BaseSearch[] { LinearSearch, BinarySearch };
 
-		}
+            new Thread(() => Simulate(searches)).Start();
+            IsSimulating = false;
+        }
 
-		private async void Simulate(params BaseSearch[] searchTypes)
-		{
-			IsSimulating = true;
-			searchTypes[0].InitializeData();
-			searchTypes[1].InitializeData();
+        private void Cancel()
+        {
 
-			Entries = GetEntries();
-			LinearSearchResults = await SimulateLinearSearchAsync(searchTypes[0]).ConfigureAwait(true);
-			BinarySearchResults = await SimulateBinarySearchAsync(searchTypes[1]).ConfigureAwait(true);
-			IsSimulating = false;
-		}
+        }
 
-		private Task<ISimulationResults> SimulateLinearSearchAsync(BaseSearch linearSearch)
-		{
-			return Task.Factory.StartNew(() =>
-			{
-				var totalNoOfIterations = 0.0;
-				var start = DateTime.Now;
-				for (var j = 0; j < NoOfSearches; j++)
-				{
-					var value = BaseSearch.Random.Next(linearSearch.StartValue, linearSearch.EndValue);
-					var searchItem = linearSearch.FindItem(value);
-					totalNoOfIterations += searchItem.NoOfIterations;
-					ProgressBarValue = (j + 1) * 100.0 / NoOfSearches;
-				}
-				var end = DateTime.Now;
-				ProgressBarValue = 0;
-				ProgressBarVisibility = Visibility.Collapsed;
+        private async void Simulate(params BaseSearch[] searchTypes)
+        {
+            IsSimulating = true;
+            searchTypes[0].InitializeData();
+            searchTypes[1].InitializeData();
 
-				var totalElapsedTime = (end - start).TotalMilliseconds;
-				return SimulationResults(totalNoOfIterations, totalElapsedTime);
-			});
-		}
+            Entries = GetEntries();
+            LinearSearchResults = await SimulateLinearSearchAsync(searchTypes[0]).ConfigureAwait(true);
+            BinarySearchResults = await SimulateBinarySearchAsync(searchTypes[1]).ConfigureAwait(true);
 
-		private Task<ISimulationResults> SimulateBinarySearchAsync(BaseSearch binarySearch)
-		{
-			return Task.Factory.StartNew(() =>
-			{
-				TargetValue = binarySearch[0];
-				var entries = binarySearch.FindItem(TargetValue);
-				TargetIndex = entries.TargetIndex;
+            LinearAvgNoOfIterations = LinearSearchResults.AvgNoOfIterations;
+            LinearAvgElapsedTime = LinearSearchResults.AvgElapsedTime;
 
-				var start = DateTime.Now;
-				var totalNoOfIterations = 0.0;
-				for (var j = 0; j < NoOfSearches; j++)
-				{
-					var value = BaseSearch.Random.Next(binarySearch.StartValue, binarySearch.EndValue);
-					var searchItem = binarySearch.FindItem(value);
-					totalNoOfIterations += searchItem.NoOfIterations;
-				}
-				var end = DateTime.Now;
+            BinaryAvgNoOfIterations = BinarySearchResults.AvgNoOfIterations;
+            BinaryAvgElapsedTime = BinarySearchResults.AvgElapsedTime;
+            IsSimulating = false;
+        }
 
-				var totalElapsedTime = (end - start).TotalMilliseconds;
-				return SimulationResults(totalNoOfIterations, totalElapsedTime);
-			});
-		}
+        private Task<ISimulationResults> SimulateLinearSearchAsync(BaseSearch linearSearch)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var totalNoOfIterations = 0.0;
+                var stopwatch = Stopwatch.StartNew();
+                for (var j = 0; j < NoOfSearches; j++)
+                {
+                    var value = BaseSearch.Random.Next(linearSearch.StartValue, linearSearch.EndValue);
+                    var searchItem = linearSearch.FindItem(value);
+                    totalNoOfIterations += searchItem.NoOfIterations;
+                    ProgressBarValue = (j + 1) * 100.0 / NoOfSearches;
+                }
+                stopwatch.Stop();
+                var timeInSec = (double)stopwatch.ElapsedMilliseconds / 1000;
+                ProgressBarValue = 0;
+                ProgressBarVisibility = Visibility.Collapsed;
 
-		private ISimulationResults SimulationResults(double totalNoOfIterations, double totalElapsedTime)
-		{
-			return new SimulationResults()
-			{
-				NoOfEntries = NoOfEntries,
-				NoOfSearches = NoOfSearches,
-				AvgNoOfIterations = totalNoOfIterations / NoOfSearches,
-				AvgElapsedTime = totalElapsedTime / NoOfSearches
-			};
-		}
+                var elapsedTimeInSec = Math.Round(timeInSec, 1);
+                return SimulationResults(totalNoOfIterations, elapsedTimeInSec);
+            });
+        }
 
-		private string GetEntries()
-		{
-			if (NoOfEntries <= 32)
-			{
-				return GetSubString(0, NoOfEntries - 1).ToString();
-			}
+        private Task<ISimulationResults> SimulateBinarySearchAsync(BaseSearch binarySearch)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var totalNoOfIterations = 0.0;
+                var stopwatch = Stopwatch.StartNew();
+                for (var j = 0; j < NoOfSearches; j++)
+                {
+                    var value = BaseSearch.Random.Next(binarySearch.StartValue, binarySearch.EndValue);
+                    var searchItem = binarySearch.FindItem(value);
+                    totalNoOfIterations += searchItem.NoOfIterations;
+                    ProgressBarValue = 100 * (j + 1) / NoOfSearches;
+                }
+                stopwatch.Stop();
+                var timeInSec = (double)stopwatch.ElapsedMilliseconds / 1000;
+                ProgressBarValue = 0;
+                ProgressBarVisibility = Visibility.Collapsed;
 
-			const string subEtc = " ..., ";
-			var mid = NoOfEntries / 2;
+                var elapsedTimeInSec = Math.Round(timeInSec, 5);
+                return SimulationResults(totalNoOfIterations, elapsedTimeInSec);
+            });
+        }
 
-			var startStr = GetSubString(0, 4);
-			var midStr = GetSubString(mid, mid + 2);
-			var endStr = GetSubString(NoOfEntries - 2, NoOfEntries - 1);
+        private ISimulationResults SimulationResults(double totalNoOfIterations, double totalElapsedTime)
+        {
+            return new SimulationResults()
+            {
+                NoOfEntries = NoOfEntries,
+                NoOfSearches = NoOfSearches,
+                AvgNoOfIterations = totalNoOfIterations / NoOfSearches,
+                AvgElapsedTime = totalElapsedTime / NoOfSearches
+            };
+        }
 
-			return startStr.Append(subEtc).Append(midStr).Append(subEtc).Append(endStr).ToString();
-		}
+        private string GetEntries()
+        {
+            if (NoOfEntries <= 32)
+            {
+                return GetSubString(0, NoOfEntries - 1).ToString();
+            }
 
-		private StringBuilder GetSubString(int fromIndex, int toIndex)
-		{
-			var sb = new StringBuilder();
-			for (var i = fromIndex; i < toIndex; i++)
-			{
-				sb.Append(BinarySearchType[i] + ", ");
-			}
+            const string subEtc = " ..., ";
+            var mid = NoOfEntries / 2;
 
-			return (toIndex == NoOfEntries - 1)
-				? sb.Append(BinarySearchType[toIndex])
-				: sb.Append(BinarySearchType[toIndex] + ", ");
-		}
+            var startStr = GetSubString(0, 4);
+            var midStr = GetSubString(mid, mid + 2);
+            var endStr = GetSubString(NoOfEntries - 2, NoOfEntries - 1);
 
-		private bool IsInputValid()
-		{
-			Validate();
-			return !HasErrors;
-		}
+            return startStr.Append(subEtc).Append(midStr).Append(subEtc).Append(endStr).ToString();
+        }
 
-		/***************************************** Private Fields ******************************************/
-		private int _targetValue;
+        private StringBuilder GetSubString(int fromIndex, int toIndex)
+        {
+            var sb = new StringBuilder();
+            for (var i = fromIndex; i < toIndex; i++)
+            {
+                sb.Append(BinarySearch[i] + ", ");
+            }
 
-		private int _noOfEntries;
-		private int _noOfSearches;
-		private bool _isSimulating;
-		private Visibility _progressBarVisibility;
-		private double _progressBarValue;
-		private int? _targetIndex;
-	}
+            return (toIndex == NoOfEntries - 1)
+                ? sb.Append(BinarySearch[toIndex])
+                : sb.Append(BinarySearch[toIndex] + ", ");
+        }
+
+        private bool IsInputValid()
+        {
+            Validate();
+            return !HasErrors;
+        }
+
+        private LinearSearch LinearSearch { get; set; }
+
+        private BinarySearch BinarySearch { get; set; }
+
+        /***************************************** Private Fields ******************************************/
+        private int _targetValue;
+
+        private int _noOfEntries;
+        private int _noOfSearches;
+        private bool _isSimulating;
+        private Visibility _progressBarVisibility;
+        private double _progressBarValue;
+        private int? _targetIndex;
+        private double _linearAvgNoOfIterations;
+        private double _linearAvgElapsedTime;
+        private double _binaryAvgNoOfIterations;
+        private double _binaryAvgElapsedTime;
+    }
 }
